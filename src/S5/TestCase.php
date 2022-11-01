695b420e61c110c3f34f851da35a1f8cfb3d63cc
Ser5 <ser5lexx@gmail.com>
2022-11-01 13:58:12 +0500

    S5/TestCase - правки сигнатур

--- src/S5/TestCase.php
+++ src/S5/TestCase.php
@@ -1,8 +1,10 @@
 <?
 namespace S5;
+use S5\IO\Directory;
+
 
 class TestCase extends \PHPUnit\Framework\TestCase {
-	public function assertException ($func, $failText = 'Failed') {
+	public function assertException ($func, string $failText = 'Failed') {
 		try {
 			$func();
 			$this->fail($failText);
@@ -10,4 +12,12 @@ class TestCase extends \PHPUnit\Framework\TestCase {
 			$this->assertTrue(true); //Чтобы PHPUnit не пищал про Risky tests
 		}
 	}
+
+
+
+
+	/*public function getDirectoryFromCurrent (string $dirPath): string {
+		$callerDirPath = dirname(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)[0]['file']);
+		return new Directory("$callerDirPath/$dirPath");
+	}*/
 }
