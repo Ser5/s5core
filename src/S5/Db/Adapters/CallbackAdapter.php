d97fdfcd2a7cd3940fc330c7b3c4eb2a4cff60ed
Ser5 <ser5lexx@gmail.com>
2022-11-08 19:55:34 +0500

    CallbackAdapter: в callback аргументы передаются не хэшем, а массивом

--- src/S5/Db/Adapters/CallbackAdapter.php
+++ src/S5/Db/Adapters/CallbackAdapter.php
@@ -11,52 +11,52 @@ class CallbackAdapter extends AbstractAdapter {
 
 
 	public function escape (string $string): string {
-		return call_user_func($this->callback, 'escape', compact('string'));
+		return call_user_func($this->callback, 'escape', [$string]);
 	}
 
 
 
 	public function query (string $query) {
-		return call_user_func($this->callback, 'query', compact('query'));
+		return call_user_func($this->callback, 'query', [$query]);
 	}
 
 
 
 	public function fetchObject ($r) {
-		return call_user_func($this->callback, 'fetchObject', ['r' => null]);
+		return call_user_func($this->callback, 'fetchObject', [$r]);
 	}
 
 	public function fetchAssoc ($r) {
-		return call_user_func($this->callback, 'fetchAssoc', ['r' => null]);
+		return call_user_func($this->callback, 'fetchAssoc', [$r]);
 	}
 
 
 
 	public function getObject (string $query) {
-		return call_user_func($this->callback, 'getObject', compact('query'));
+		return call_user_func($this->callback, 'getObject', [$query]);
 	}
 
 	public function getObjectsList (string $query): array {
-		return call_user_func($this->callback, 'getObjectsList', compact('query'));
+		return call_user_func($this->callback, 'getObjectsList', [$query]);
 	}
 
 
 
 	public function getAssoc (string $query) {
-		return call_user_func($this->callback, 'getAssoc', compact('query'));
+		return call_user_func($this->callback, 'getAssoc', [$query]);
 	}
 
 	public function getAssocList (string $query): array {
-		return call_user_func($this->callback, 'getAssocList', compact('query'));
+		return call_user_func($this->callback, 'getAssocList', [$query]);
 	}
 
 
 
 	public function getInsertId (): int {
-		return call_user_func($this->callback, 'getInsertId');
+		return call_user_func($this->callback, 'getInsertId', []);
 	}
 
 	public function getAffectedRows (): int {
-		return call_user_func($this->callback, 'getAffectedRows');
+		return call_user_func($this->callback, 'getAffectedRows', []);
 	}
 }
