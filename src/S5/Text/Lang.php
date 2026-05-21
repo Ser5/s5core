c5778573ae622a98cb193665d16bc4a29909f6da
Ser5 <ser5lexx@gmail.com>
2026-05-21 11:03:44 +0500

    S5\Text\Lang - форматирование телефонов

--- src/S5/Text/Lang.php
+++ src/S5/Text/Lang.php
@@ -1,12 +1,17 @@
 <?
 namespace S5\Text;
 
+use libphonenumber\PhoneNumberUtil;
+
 
 
 class Lang {
+	protected PhoneNumberUtil $phoneFormatter;
+
 	public function __construct (
 		protected string $locale = 'ru',
 	) {
+		$this->phoneFormatter = PhoneNumberUtil::getInstance();
 	}
 
 
@@ -40,9 +45,12 @@ class Lang {
 	 * $lang->pluralize(5, 'unit',  'units'); //5 units
 	 *
 	 * //Три варианта
-	 * $lang->pluralize(1, 'штука', 'штуки', 'штук');  //1 штука
-	 * $lang->pluralize(3, 'штука', 'штуки', 'штук');  //3 штуки
-	 * $lang->pluralize(5, 'штука', 'штуки', 'штук');  //5 штук
+	 * $lang->pluralize(1, 'штука', 'штуки', 'штук');   //1 штука
+	 * $lang->pluralize(3, 'штука', 'штуки', 'штук');   //3 штуки
+	 * $lang->pluralize(5, 'штука', 'штуки', 'штук');   //5 штук
+	 * $lang->pluralize(1, 'unit',  'units', 'unitaz'); //1 unit
+	 * $lang->pluralize(3, 'unit',  'units', 'unitaz'); //3 units
+	 * $lang->pluralize(5, 'unit',  'units', 'unitaz'); //5 units
 	 * ```
 	 */
 	public function pluralize (int $count, string $variant1 = '', string $variant2 = '', string $variant3 = ''): string {
@@ -62,5 +70,22 @@ class Lang {
 		}
 
 		return $this->format($pattern, ['count' => $count]);
+	}
+
+
+
+	public function formatPhone (string $phone): string {
+		$proto = $this->phoneFormatter->parse($phone, $this->locale);
+		$r     = $this->phoneFormatter->formatOutOfCountryCallingNumber($proto, $this->locale);
+		//libphonenumber\PhoneNumberUtil для России может возвращать результат в виде:
+		//- +79871234567
+		//- 8 (987) 123-45-67
+		//- +7 987 123-45-67
+		//Тогда как нам нужно +7 (987) 123-45-67.
+		//Заменяем "8" на "+7".
+		if ($this->locale == 'ru' and $r[0] == '8') {
+			$r = substr_replace($r, '+7', 0, 1);
+		}
+		return $r;
 	}
 }
